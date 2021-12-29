8d98f051ca1f4d9b97db1c5255a3b63a420b7ee4
Nikodem Janik <nikodemj@gmail.com>
2021-12-29 22:49:15 +0100

    Split dataset into train, validation, test sets

--- create_splits.py
+++ create_splits.py
@@ -17,7 +17,46 @@ def split(source, destination):
         - source [str]: source data directory, contains the processed tf records
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
-    # TODO: Implement function
+    # Create folders, catch exception if folder is already created
+    try:
+        os.mkdir(destination+"/train")
+    except OSError as error:
+        print(error)
+    try:
+        os.mkdir(destination+"/val")
+    except OSError as error:
+        print(error)
+    try:
+        os.mkdir(destination+"/test")
+    except OSError as error:
+        print(error)
+
+    # Get list of all files and shuffle
+    files = os.listdir(source)
+    random.shuffle(files) 
+
+    # Split into 70-15-15
+    files_100 = len(files)
+    files_70 = int(0.7 * len(files))
+    files_85 = int(0.85 * len(files))
+
+    # Move training set (0-70%)
+    for i in range(files_70):
+        original_file = source+'/'+files[i]
+        moved_file = destination+"/train/"+files[i]
+        os.rename(original_file, moved_file)
+
+    # Move evaluation set (70-85%)
+    for i in range(files_70, files_85):
+        original_file = source+'/'+files[i]
+        moved_file = destination+"/val/"+files[i]
+        os.rename(original_file, moved_file)
+
+    # Move test set (85-100%)
+    for i in range(files_85, files_100):
+        original_file = source+'/'+files[i]
+        moved_file = destination+"/test/"+files[i]
+        os.rename(original_file, moved_file)
 
 
 if __name__ == "__main__":
